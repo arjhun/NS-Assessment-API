433538f06453d04adcb240f9e918e1fb6ff72adb
arjhun <arjenklaverstijn@gmail.com>
2025-02-12 17:28:34 +0100

    Initial commit

---
+++ build/server.d.ts
@@ -0,0 +1,2 @@
+export declare const endpoint1: (message: string) => void;
+//# sourceMappingURL=server.d.ts.map
---
+++ build/server.js
@@ -0,0 +1,4 @@
+export const endpoint1 = (message) => {
+    console.log(message);
+};
+endpoint1("hello");
